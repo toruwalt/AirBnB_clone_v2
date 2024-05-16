50ff61bec52e8b283f92f2c767645bf0d13e98a6
Torubiri Walter <toruwalt1997@gmail.com>
2024-05-17 00:17:02 +0100

    Web Flask - Task 5

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+"""Write a script that starts a Flask web application
+
+    The application listens on 0.0.0.0, port 5000.
+    Routes:
+        /: Displays 'Hello HBNB!'.
+        /hbnb: Displays 'HBNB'.
+        /c/<text>: Displays 'C' followed by the value of <text>.
+        /python/(<text>): Displ 'Python' followed by the value of <text>.
+"""
+
+from flask import Flask, render_template
+from markupsafe import escape
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello():
+    return 'Hello HBNB!'
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb_hbnb():
+    return 'HBNB'
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def hbnb_hbnb_c(text):
+    return 'C {}'.format(escape(text)).replace('_', ' ')
+
+
+@app.route("/python", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def hbnb_hbnb_python(text='is cool'):
+    return 'Python {}'.format(escape(text)).replace('_', ' ')
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def hbnb_hbnb_number(n):
+    return '{} is a number'.format(escape(n))
+
+
+@app.route("/number_template/<int:n>", strict_slashes=False)
+def hbnb_hbnb_number_templates(n):
+    return render_template('5-number.html', n=n)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
