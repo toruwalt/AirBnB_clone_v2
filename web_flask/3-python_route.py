c726284cb1838350cea6969345b08f47ba28b981
Torubiri Walter <toruwalt1997@gmail.com>
2024-05-17 00:31:56 +0000

    Web Flash - Task 3:Added Right Documentation

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -18,22 +18,26 @@ app = Flask(__name__)
 
 @app.route("/", strict_slashes=False)
 def hello():
+    """Return Hello HBNB"""
     return 'Hello HBNB!'
 
 
 @app.route("/hbnb", strict_slashes=False)
 def hbnb_hbnb():
+    """Return HBNB"""
     return 'HBNB'
 
 
 @app.route("/c/<text>", strict_slashes=False)
 def hbnb_hbnb_c(text):
+    """C is fun"""
     return 'C {}'.format(escape(text)).replace('_', ' ')
 
 
 @app.route("/python", strict_slashes=False)
 @app.route("/python/<text>", strict_slashes=False)
 def hbnb_hbnb_python(text='is cool'):
+    """Return Python is cool"""
     return 'Python {}'.format(escape(text)).replace('_', ' ')
 
 
