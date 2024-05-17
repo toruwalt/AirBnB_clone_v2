a28b8c1534fbc72e2b09fdb73ec3bba769fab10b
Torubiri Walter <toruwalt1997@gmail.com>
2024-05-17 00:35:15 +0000

    Web Flash - Task 5:Added Right Documentation

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -19,32 +19,38 @@ app = Flask(__name__)
 
 @app.route("/", strict_slashes=False)
 def hello():
+    """Return Hello HBNB"""
     return 'Hello HBNB!'
 
 
 @app.route("/hbnb", strict_slashes=False)
 def hbnb_hbnb():
+    """Return HBNB"""
     return 'HBNB'
 
 
 @app.route("/c/<text>", strict_slashes=False)
 def hbnb_hbnb_c(text):
+    """Return C is fun"""
     return 'C {}'.format(escape(text)).replace('_', ' ')
 
 
 @app.route("/python", strict_slashes=False)
 @app.route("/python/<text>", strict_slashes=False)
 def hbnb_hbnb_python(text='is cool'):
+    """Return Python is cool"""
     return 'Python {}'.format(escape(text)).replace('_', ' ')
 
 
 @app.route("/number/<int:n>", strict_slashes=False)
 def hbnb_hbnb_number(n):
+    """Return A number"""
     return '{} is a number'.format(escape(n))
 
 
 @app.route("/number_template/<int:n>", strict_slashes=False)
 def hbnb_hbnb_number_templates(n):
+    """Return HTML Number Page"""
     return render_template('5-number.html', n=n)
 
 
